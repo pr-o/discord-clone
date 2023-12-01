fef654ed9f8744eb855f960f2bc63b18070e79a7
pr-o <sunghahh.git@gmail.com>
2023-12-01 18:16:02 +0900

    add react-query

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { Open_Sans } from "next/font/google"
 import { ClerkProvider } from "@clerk/nextjs"
 
 import { cn } from "@/lib/utils"
+import QueryProvider from "@/components/providers/queryProvider"
 import { ThemeProvider } from "@/components/providers/theme-provider"
 
 const font = Open_Sans({ subsets: ["latin"] })
@@ -23,14 +24,16 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem={false}
-            storageKey="discord-clone-by-sung-theme"
-          >
-            {children}
-          </ThemeProvider>
+          <QueryProvider>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem={false}
+              storageKey="discord-clone-by-sung-theme"
+            >
+              {children}
+            </ThemeProvider>
+          </QueryProvider>
         </body>
       </html>
     </ClerkProvider>
