78377883f14b3a714e043197eff35269295f7c4f
pr-o <sunghahh.git@gmail.com>
2023-12-07 19:53:16 +0900

    UI for channels & chat

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next"
 import { Open_Sans } from "next/font/google"
 import { ModalProvider } from "@/providers/modal-provider"
 import { QueryProvider } from "@/providers/query-provider"
+import { SocketProvider } from "@/providers/socket-provider"
 import { ThemeProvider } from "@/providers/theme-provider"
 import { ClerkProvider } from "@clerk/nextjs"
 
@@ -27,16 +28,18 @@ export default function RootLayout({
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
           <QueryProvider>
-            <ThemeProvider
-              attribute="class"
-              defaultTheme="dark"
-              enableSystem={false}
-              storageKey="discord-clone-by-sung-theme"
-            >
-              {children}
-              <ModalProvider />
-              <Toaster />
-            </ThemeProvider>
+            <SocketProvider>
+              <ThemeProvider
+                attribute="class"
+                defaultTheme="dark"
+                enableSystem={false}
+                storageKey="discord-clone-by-sung-theme"
+              >
+                {children}
+                <ModalProvider />
+                <Toaster />
+              </ThemeProvider>
+            </SocketProvider>
           </QueryProvider>
         </body>
       </html>
